aed25fba95a5d9395c3957390399922dcdca9b61
graceng-yl <yeelinng28@gmail.com>
2022-03-17 17:49:09 +0800

    advsearch1

--- pages/header.php
+++ pages/header.php
@@ -1,7 +1,13 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'medipherd');
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+    // connect the MySQL server
+    $conn = mysqli_connect("localhost", "root", "");
+    if(!$conn){
+        die("Could not connect to the server</body></html>"); 
+    }
+
+    // access the database
+    if(!mysqli_select_db($conn, "medipherd")){
+        die("Could not connect to the database</body></html>"); 
     }
 ?>
 
@@ -13,8 +19,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- <link rel="icon" href=""> -->
 
-        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!-- <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
         <script src="js/script.js"></script>
 
