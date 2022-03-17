8be134a23aba3e547fe3beb1e6f6bd8abcaa6fee
Pendy <pendytok0518@gmail.com>
2022-03-17 18:22:03 +0800

    Draft header and footer

--- pages/footer.php
+++ pages/footer.php
@@ -1,5 +1,12 @@
         <footer>
+            <div class="container text-center">
+                <p>
+                    &copy; All Rights Reserved. Designed and Developed by Group x <br>
+                    TWT2231 Project
+                </p>
+            </div>
 
         </footer>
-    </body>
-</html>
+        </body>
+
+        </html>
