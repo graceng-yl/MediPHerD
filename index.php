4595330a70389ac435ee8b0f0b32c1ed33d6658c
Pendy <pendytok0518@gmail.com>
2022-03-23 00:51:43 +0800

    draft header and index php; doing simple search

--- index.php
+++ index.php
@@ -2,6 +2,39 @@
 include('header.php');
 ?>
 
+<div class="container py-5">
+    <div class="d-flex justify-content-center align-items-center">
+        <p class="mx-auto" style="font-size: var(--font_xlarge);">MediPHerD</p>
+    </div>
+
+    <div class="d-flex justify-content-center align-items-center">
+        <p>Malaysia Medical Plants and Herbs Database</p>
+    </div>
+
+    <div class="row height d-flex justify-content-center align-items-center">
+        <div class="col-md-3 ">
+            <div class="search "> <i class="fa fa-search "></i> <input type="text" class="form-control rounded-pill"
+                    placeholder="Search">
+
+                <!-- <button class="btn btn-primary">Search</button>  -->
+            </div>
+        </div>
+    </div>
+
+    <div class="container d-flex justify-content-center align-items-center py-5">
+        <div style="text-align:justify;width: 60%;">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda odio illo, officia temporibus cumque
+                nobis ipsa repellat commodi facilis aliquid totam eius, reprehenderit aut incidunt iste ea iure,
+                suscipit
+                ipsum.</p>
+        </div>
+
+    </div>
+    <!-- <div class="input-box"> <input type="text" class="form-control"> <i class="fa fa-search"></i> </div> -->
+
+</div>
+
+
 <?php
 include('footer.php');
 ?>
