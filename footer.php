0e21de9030eb0f1185e782219d7ef489eca7e2e9
Pendy Tok <74284859+pendy05@users.noreply.github.com>
2022-03-23 17:33:13 +0800

    Revert "draft header and index php; doing simple search"

--- footer.php
+++ footer.php
@@ -1,10 +1,8 @@
         <footer>
-            <div class="text-center py-4" style="background-color: var(--c_darkgreen);">
-                <p style="color: var(--c_white);">
+            <div class="container-pendy text-center">
+                <p>
                     &copy; All Rights Reserved. Designed and Developed by Group x <br>
                     TWT2231 Project
-                    <br>
-                    -- For education purpose only --
                 </p>
             </div>
 
--- header.php
+++ header.php
@@ -22,77 +22,32 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
     <script src="js/script.js"></script>
 
     <!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css">
         -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
-    <script>
-    //press 'enter' key to initiate the simple search
-    $(document).on("keypress", "input", function(e) {
-        if (e.which == 13 && $(this).val() != "") {
-            var inputVal = $(this).val();
-            //alert("You've entered: " + inputVal);
-            $.post('header.php', 'searchinput=' + inputVal, function(response) {
-                alert(response);
-            });
-        } else if (e.which == 13 && $(this).val() == "") {
-            alert("You've entered: nothing");
-        }
-    });
-    </script>
 </head>
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg justify-content-between" style="background-color: var(--c_darkgreen);">
-            <ul class="navbar-nav mx-auto ">
-                <li class="nav-item my-auto">
-                    <a class="nav-link active" href="index.php">Home</a>
-                </li>
-                <li class="nav-item my-auto">
-                    <a class="nav-link" href="browse.php">Browse Database</a>
-                </li>
-                <li class="nav-item my-auto">
-                    <div class="mx-auto order-0">
-                        <a class="navbar-brand" href="index.php">
-                            <img src="www/logo.png" alt="Logo" href />
-                        </a>
+        <nav>
+            <div class="logo">
 
-                    </div>
-                </li>
-                <li class="nav-item my-auto">
-                    <form class="d-flex form-inline mx-auto">
-                        <input class="form-control me-5 rounded-pill" type="search" placeholder="Quick Search"
-                            aria-label="Search"><i class="fas fa-search" aria-hidden="true"></i>
-
-                    </form>
-                </li>
-                <li class="nav-item my-auto">
-                    <a class="nav-link" href="advsearch.php">Advanced Search</a>
-                </li>
-
-
-
-            </ul>
-
-
+            </div>
+            <div class="menu text-right">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="browse.php">Browse</a></li>
+                    <li><input type="text"></li>
+                    <li><a href="advsearch.php">Advanced Search</a></li>
+                </ul>
+            </div>
+            <div class="clearfix"></div>
         </nav>
-
-
     </header>
-
-    <?php
-   $value = $_POST['searchinput'];
-   echo "I got your value! $value";
-?>
