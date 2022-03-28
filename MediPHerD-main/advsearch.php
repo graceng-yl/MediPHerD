a6b1888a16302271232a7eafe889c32483376055
Waylynn99 <49666539+Waylynn99@users.noreply.github.com>
2022-03-28 12:17:54 +0800

    Add files via upload

---
+++ MediPHerD-main/advsearch.php
@@ -0,0 +1,80 @@
+<?php
+include('header.php');
+?>
+
+<div class="container page_content">
+    <div class="page_top">
+        <h1 class="page_title">Advanced Search</h1>
+        <p class="page_desc">Customize your search here by specifying the fields and matching criteria of your queries
+        </p>
+    </div>
+
+    <form method="post" action="searchresult.php" id="advs_form">
+        <table id="advs">
+            <tr class="advs_row" id="advs_row_1">
+                <td>
+                    <select name="advs_operator[]" id="advs_operator_1">
+                        <option value="or">OR</option>
+                        <option value="and">AND</option>
+                        <option value="not">NOT</option>
+                    </select>
+                </td>
+                <td>
+                    <select name="advs_matching_criterion[]">
+                        <option value="contains">CONTAINS</option>
+                        <option value="exact">EXACT</option>
+                        <option value="start">START WITH</option>
+                        <option value="end">END WITH</option>
+                    </select>
+                </td>
+                <td>
+                    <select name="advs_field[]">
+                        <option value="ALL">All fields</option>
+                        <option value="plant_id">ID</option>
+                        <option value="plant_name">Name</option>
+                        <option value="plant_family">Family</option>
+                        <option value="plant_genus">Genus</option>
+                        <option value="plant_species">Species</option>
+                        <option value="mat_desc">Material of interest</option>
+                        <option value="plant_usage">Usage</option>
+                        <option value="plant_chemconst">Chemical constituent</option>
+                    </select>
+                </td>
+                <td><input name="advs_query[]" type="text" placeholder="Search query" required></td>
+                <td>
+                    <div class="advs_button advs_removequery" id="advs_removequery_1">&#8722;</div>
+                </td>
+                <td>
+                    <div class="advs_button advs_addquery" id="advs_addquery_1">&#43;</div>
+                </td>
+
+            </tr>
+        </table>
+        <div class="advs_form_tail">
+            <div class="advs_form_tail_col">
+                <input type="submit" id="advs_submit">
+            </div>
+            <div class="advs_form_tail_col">
+                <input type="reset" value="Clear" id="advs_clear">
+            </div>
+        </div>
+    </form>
+
+    <div class="history_section">
+        <h2 class="subsection_title">Search History</h2>
+        <div class="search_histories">
+            <p>Find your 30 days search records here, click to edit the search queries.</p>
+            <?php
+            if(isset($_COOKIE['search_history'])){
+                foreach(array_reverse($_COOKIE['search_history']) as $history){
+                    echo "<p class='search_history'>".$history."</p>";
+                }
+            }
+?>
+        </div>
+    </div>
+</div>
+
+<?php
+include('footer.php');
+?>
---
+++ MediPHerD-main/entry.php
@@ -0,0 +1,108 @@
+<?php include('header.php'); ?>
+
+<?php
+
+$plants_query = "SELECT * FROM plants WHERE plant_id = 'P001'";
+$materials_query = "SELECT * FROM materials WHERE materials.mat_id IN (SELECT materials_relation.mat_id FROM materials_relation WHERE materials_relation.plant_id = 'P001')";
+$keywords_query = "SELECT * FROM keywords WHERE keywords.keyw_id IN (SELECT keywords_relation.keyw_id FROM keywords_relation WHERE keywords_relation.plant_id = 'P001')";
+
+$plants_result = mysqli_query($conn, $plants_query) or die(mysqli_error($conn));
+$materials_result = mysqli_query($conn, $materials_query) or die(mysqli_error($conn));
+$keywords_result = mysqli_query($conn, $keywords_query) or die(mysqli_error($conn));
+
+?>
+
+<?php
+<style>
+table, th, td {
+  border-collapse: collapse;
+}
+th, td {
+  padding-top: 10px;
+  padding-bottom: 5px;
+  padding-left: 30px;
+  padding-right: 30px;
+}
+</style>
+
+<?php while ($plant = mysqli_fetch_assoc($plants_result)) { ?>
+<div class="container py-5" id="ep">
+	<div class="page_top" id="ep">
+	<table>
+		<h1 class="page_title" id="ep_titl"><?= $plant["plant_name"] ?><h1>
+		<h2 class="page_desc" id="eptd"><?= $plant["plant_othernames"] ?></h2>
+
+		<br />
+		</div>
+		<table>
+			<tr padding= 15px>
+				<th>
+					<img src="www/Tongkat_Ali.jpg" height="300px" object-fit="scale-down"/>
+				</th>
+				<th>
+					<table>
+						<tr>
+							<th id="eptd" scope="col">Scientific Name: <br />
+							<em><b><?= $plant["plant_genus"] ?> <?= $plant["plant_species"] ?></em></b></th>
+						</tr>
+						<tr>
+							<th id="eptd" scope="col">Plant Family: <br />
+							<em><b><?= $plant["plant_family"] ?></em><b></th>
+						</tr>						
+						<tr>
+							<th id="eptd" scope="col">Material: <br />
+								<b><button id="family"><?php while ($material = mysqli_fetch_assoc($materials_result)) { ?>
+									<?= $material["mat_desc"] ?>
+								<?php } ?></button></b>
+							/*	<script type="text/javascript">
+									document.getElementById("family").onclick = function () {
+										location.href = "localhost/MediPHerD-main/entry.php?plant=[plant_id]";
+									};
+								</script>		*/						
+							</th>
+						</tr>
+						<tr>
+							<td colspan="2">
+								<?php
+								$keywords = array();
+								while ($keyword = mysqli_fetch_assoc($keywords_result)) {
+									$keywords[] = $keyword["keyw_desc"];
+								}
+								?>
+								<?= join(", ", $keywords) ?>
+							</td>
+						</tr>
+					</table>
+				</th>
+			</tr>
+		</table>
+		</div>
+		
+		<div id="ep">
+			<h2 id="eptd" class="subsection_title">Usage</h2>
+			<div id= "cat_d"><p><?= $plant["plant_usage"] ?></p></div>
+
+			<h2 id="eptd" class="subsection_title">Chemical Constituent</h2>
+			<div id= "cat_d"><p><?= $plant["plant_chemconst"] ?></p></div>
+
+			<h2 id="eptd" class="subsection_title">References</h2>
+			<div id= "cat_d"><p><?= $plant["plant_refs"] ?></p></div>
+		</div>
+		
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					1 of 2
+				</div>
+				<div class="col">
+					2 of 2
+				</div>
+			</div>
+		</div>
+		
+	</table>
+	</div>
+</div>   
+<?php } ?>
+
+<?php include('footer.php'); ?>
