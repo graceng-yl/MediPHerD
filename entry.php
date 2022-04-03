2ff2bc9ee62a8ae47c8434eff14ee0dfaeee62e7
Waylynn99 <49666539+Waylynn99@users.noreply.github.com>
2022-04-04 07:04:41 +0800

    Add files via upload
    - img dynamic
    - css done

--- entry.php
+++ entry.php
@@ -2,20 +2,7 @@
 
 <?php
 $plant_id = $_GET['plant'];
-/*$plants_query = "SELECT * FROM plants WHERE plant_id = '" . $plant_id . "';";
-if(!($plants_result = mysqli_query($conn, $query))){
-    echo "<p>Could not execute query</p>";
-    die(mysqli_error($conn)."</body.</html>");
 
-$materials_query = "SELECT * FROM materials WHERE materials.mat_id IN (SELECT materials_relation.mat_id FROM materials_relation WHERE materials_relation.plant_id = '" . $plant_id . "')";
-if(!($plants_result = mysqli_query($conn, $query))){
-    echo "<p>Could not execute query</p>";
-    die(mysqli_error($conn)."</body.</html>");
-
-$keywords_query = "SELECT * FROM keywords WHERE keywords.keyw_id IN (SELECT keywords_relation.keyw_id FROM keywords_relation WHERE keywords_relation.plant_id = '" . $plant_id . "')";
-if(!($plants_result = mysqli_query($conn, $query))){
-    echo "<p>Could not execute query</p>";
-    die(mysqli_error($conn)."</body.</html>");*/
 $plants_query = "SELECT * FROM plants WHERE plant_id = '".$plant_id."'";
 $materials_query = "SELECT * FROM materials WHERE materials.mat_id IN (SELECT materials_relation.mat_id FROM materials_relation WHERE materials_relation.plant_id = '".$plant_id."')";
 $keywords_query = "SELECT * FROM keywords WHERE keywords.keyw_id IN (SELECT keywords_relation.keyw_id FROM keywords_relation WHERE keywords_relation.plant_id = '".$plant_id."')";
@@ -29,6 +16,7 @@ $keywords_result = mysqli_query($conn, $keywords_query) or die(mysqli_error($con
 <style>
 table, th, td {
   border-collapse: collapse;
+  align-self=: center;
 }
 th, td {
   padding-top: 10px !important;
@@ -39,79 +27,70 @@ th, td {
 </style>
 
 <?php while ($plant = mysqli_fetch_assoc($plants_result)) { ?>
-<div class="mx-auto" style= "width:1500px;">
-	<div class="page_top" style="padding-top:50px" id="ep">
-	<table>
-		<h1 class="page_title" id="ep_titl"><?= $plant["plant_name"] ?><h1>
-		<h2 class="page_desc" id="eptd"><?= $plant["plant_othernames"] ?></h2>
+	<div class="mx-auto" style= "width:1500px;">
+		<div class="page_top" id="epg">
+			<table>
+				<h1 class="page_title" id="ep_titl"><?= $plant["plant_name"] ?><h1>
+				<h2 class="page_desc" id="eptd"><?= $plant["plant_othernames"] ?></h2>
 
-		<br />
-		</div>
-		<table>
-			<tr padding= 15px>
-				<th>
-					<img src="www/Tongkat_Ali.jpg" height="300px" object-fit="scale-down"/>
-				</th>
-				<th>
-					<table>
-						<tr>
-							<th id="eptd" scope="col">Scientific Name: <br />
-							<em><b><?= $plant["plant_genus"] ?> <?= $plant["plant_species"] ?></em></b></th>
-						</tr>
-						<tr>
-						
-							<th id="eptd" scope="col">Plant Family: <br />
-							<b><?= $plant["plant_family"] ?><b></th>
-						</tr>						
-						<tr>
-							<th id="eptd" scope="col">Material: <br />
-								<b><?php while ($material = mysqli_fetch_assoc($materials_result)) { ?>
-									<?= $material["mat_desc"] ?>
-								<?php } ?></b>						
-							</th>
-						</tr>
-						<tr>
-							<td colspan="2">
-								<?php
-								$keywords = array();
-								while ($keyword = mysqli_fetch_assoc($keywords_result)) {
-									$keywords[] = $keyword["keyw_desc"];
-								}
-								?>
-								<?= join(", ", $keywords) ?>
-							</td>
-						</tr>
-					</table>
-				</th>
-			</tr>
-		</table>
-		</div>
-		
-		<div id="ep">
-			<h2 id="eptd" class="subsection_title">Usage</h2>
-			<div id= "cat_d"><p><?= $plant["plant_usage"] ?></p></div>
+				</br>
 
-			<h2 id="eptd" class="subsection_title">Chemical Constituent</h2>
-			<div id= "cat_d"><p><?= $plant["plant_chemconst"] ?></p></div>
-
-			<h2 id="eptd" class="subsection_title">References</h2>
-			<div id= "cat_d"><p><?= $plant["plant_refs"] ?></p></div>
-		</div>
-		
-		<div class="container">
-			<div class="row">
-				<div class="col">
-					1 of 2
+				<table id="epf">
+					<tr padding= 15px>
+						<th>				
+							<img src="www/<?= $plant["plant_id"]?>.jpg" id="text" alt=<?=$plant["plant_name"] ?> height="300px" object-fit="scale-down"/>
+						</th>
+						<th>
+							<table>
+								<tr>
+									<th id="eptd" scope="col">Scientific Name: <br />
+									&nbsp&nbsp&nbsp<em><b><?= $plant["plant_genus"] ?> <?= $plant["plant_species"] ?></em></b></th>
+								</tr>
+								<tr>
+								
+									<th id="eptd" scope="col">Plant Family: <br />
+									&nbsp&nbsp&nbsp<b><?= $plant["plant_family"] ?><b></th>
+								</tr>						
+								<tr>
+									<th id="eptd" scope="col">Material: <br />
+									&nbsp&nbsp&nbsp<b><?php while ($material = mysqli_fetch_assoc($materials_result)) { ?>
+											<?= $material["mat_desc"] ?>
+										<?php } ?></b>						
+									</th>
+								</tr>
+								<tr>
+									<td colspan="2">
+										<?php
+										$keywords = array();
+										while ($keyword = mysqli_fetch_assoc($keywords_result)) {
+											$keywords[] = $keyword["keyw_desc"];
+										}
+										?>
+										<?= join(", ", $keywords) ?>
+									</td>
+								</tr>
+							</table>
+						</th>
+					</tr>
+				</table>
 				</div>
-				<div class="col">
-					2 of 2
-				</div>
-			</div>
+				
+				<div id="ep">
+					<h2 id="eptd" class="subsection_title">Usage</h2>
+					<div id= "cat_d"><p><?= $plant["plant_usage"] ?></p></div>
+
+					<h2 id="eptd" class="subsection_title">Chemical Constituent</h2>
+					<div id= "cat_d"><p><?= $plant["plant_chemconst"] ?></p></div>
+
+					<h2 id="eptd" class="subsection_title">References</h2>
+					<div id= "cat_d"><p><?= $plant["plant_refs"] ?></p></div>
+				</div><br/>
+				
+
+				
+			</table>
 		</div>
-		
-	</table>
-	</div>
-</div>   
+	</div>   
 <?php } ?>
 
 <?php include('footer.php'); ?>
