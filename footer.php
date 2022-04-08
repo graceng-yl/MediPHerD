aabc82b6c609754b2381ce7141815407f4a33fa9
graceng-yl <yeelinng28@gmail.com>
2022-04-08 09:16:59 +0800

    footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
         <footer class="text-center py-4 footer">
             <div class="footer_text">
                 <b>Copyright &copy; 2022 MediPHerD. All Rights Reserved.</b>
-                <p>Developed by Grace Ng Yee Lin, Pendy Tok, Phuah Way Lynn, and Yap Wan Yi</p>
+                <p>Developed by Grace Ng Yee Lin, Pendy Tok, and Phuah Way Lynn</p>
                 <p>TWT2231 Web Techniques and Applications - Project</p>
                 <i>-- For education purpose only --</i>
             </div>
